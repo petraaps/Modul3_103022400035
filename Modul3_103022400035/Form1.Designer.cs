306152e0b4f08f6f8f5a6a6eb7f6c87c2892bcad
petraaps <petrasimangunsong07@gmail.com>
2026-03-14 09:05:02 +0700

    Menambahkan tampilan GUI

--- Modul3_103022400035/Form1.Designer.cs
+++ Modul3_103022400035/Form1.Designer.cs
@@ -28,12 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(83, 50);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(92, 28);
+            comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(83, 140);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(92, 28);
+            comboBox2.TabIndex = 1;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(225, 51);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(255, 27);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(225, 141);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(255, 27);
+            textBox2.TabIndex = 3;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(500, 51);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 4;
+            button1.Text = "Convert";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(83, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 20);
+            label1.TabIndex = 5;
+            label1.Text = "Satuan Awal";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(83, 118);
+            label2.Name = "label2";
+            label2.Size = new Size(92, 20);
+            label2.TabIndex = 6;
+            label2.Text = "Satuan Akhir";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(226, 28);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Nilai Awal";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(225, 118);
+            label4.Name = "label4";
+            label4.Size = new Size(78, 20);
+            label4.TabIndex = 8;
+            label4.Text = "Nilai Akhir";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
--- Modul3_103022400035/Form1.cs
+++ Modul3_103022400035/Form1.cs
@@ -5,6 +5,121 @@ namespace Modul3_103022400035
         public Form1()
         {
             InitializeComponent();
+            string[] satuan = { "Celcius", "Fahrenheit", "Kelvin", "Reamur" };
+            comboBox1.Items.AddRange(satuan);
+            comboBox2.Items.AddRange(satuan);
+            textBox2.ReadOnly = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private string ConvertTemperature(string input, string satuan1, string satuan2)
+        {
+            double value = double.Parse(input);
+            double result = 0;
+            switch (satuan1)
+            {
+                case "Celcius":
+                    if (satuan2 == "Fahrenheit")
+                        result = value * 9 / 5 + 32;
+                    else if (satuan2 == "Kelvin")
+                        result = value + 273.15;
+                    else if (satuan2 == "Reamur")
+                        result = value * 4 / 5;
+                    else
+                        result = value;
+                    break;
+                case "Fahrenheit":
+                    if (satuan2 == "Celcius")
+                        result = (value - 32) * 5 / 9;
+                    else if (satuan2 == "Kelvin")
+                        result = (value - 32) * 5 / 9 + 273.15;
+                    else if (satuan2 == "Reamur")
+                        result = (value - 32) * 4 / 9;
+                    else
+                        result = value;
+                    break;
+                case "Kelvin":
+                    if (satuan2 == "Celcius")
+                        result = value - 273.15;
+                    else if (satuan2 == "Fahrenheit")
+                        result = (value - 273.15) * 9 / 5 + 32;
+                    else if (satuan2 == "Reamur")
+                        result = (value - 273.15) * 4 / 5;
+                    else
+                        result = value;
+                    break;
+                case "Reamur":
+                    if (satuan2 == "Celcius")
+                        result = value * 5 / 4;
+                    else if (satuan2 == "Fahrenheit")
+                        result = value * 9 / 4 + 32;
+                    else if (satuan2 == "Kelvin")
+                        result = value * 5 / 4 + 273.15;
+                    else
+                        result = value;
+                    break;
+            }
+            return result.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null || comboBox2.SelectedItem == null)
+            {
+                MessageBox.Show("Silahkan pilih satuan");
+                return;
+            }
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Masukkan angka");
+                return;
+            }
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "Celcius":
+                    textBox2.Text = ConvertTemperature(textBox1.Text, "Celcius", comboBox2.SelectedItem.ToString());
+                    break;
+                case "Fahrenheit":
+                    textBox2.Text = ConvertTemperature(textBox1.Text, "Fahrenheit", comboBox2.SelectedItem.ToString());
+                    break;
+                case "Kelvin":
+                    textBox2.Text = ConvertTemperature(textBox1.Text, "Kelvin", comboBox2.SelectedItem.ToString());
+                    break;
+                case "Reamur":
+                    textBox2.Text = ConvertTemperature(textBox1.Text, "Reamur", comboBox2.SelectedItem.ToString());
+                    break;
+                default:
+                    MessageBox.Show("Satuan tidak valid");
+                    break;
+            }
         }
     }
 }
